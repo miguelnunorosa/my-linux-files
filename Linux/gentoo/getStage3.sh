efa03bcb021dabdf81482552ca0146608e8324a1
Mike <46021063+miguelnunorosa@users.noreply.github.com>
2022-03-09 11:05:04 +0000

    Update getStage3.sh

--- Linux/gentoo/getStage3.sh
+++ Linux/gentoo/getStage3.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+URL='https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds'
+DATE='20220308T160629Z'
+
+
+
 #OpenRC
-STAGE3_OPENRC="https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20210627T214502Z/stage3-amd64-20210627T214502Z.tar.xz"
+STAGE3_OPENRC="${URL}/${DATE}/stage3-amd64-desktop-openrc-${DATE}.tar.xzz"
 
 #SystemD
-STAGE3_SYSTEMD="https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20210627T214502Z/stage3-amd64-systemd-20210627T214502Z.tar.xz"
+STAGE3_SYSTEMD="${URL}/${DATE}/stage3-amd64-desktop-systemd-${DATE}.tar.xz"
 
 
 wget $STAGE3_OPENRC

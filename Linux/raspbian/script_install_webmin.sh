a53880beeba309d10ed2f59062705ba8d0517cfc
Mike <46021063+miguelnunorosa@users.noreply.github.com>
2022-03-09 11:11:16 +0000

    Update script_install_webmin.sh

--- Linux/raspbian/script_install_webmin.sh
+++ Linux/raspbian/script_install_webmin.sh
@@ -28,5 +28,10 @@ readonly DEPENDENCIES_LIST="perl libnet-ssleay-perl openssl libauthen-pam-perl l
 #INSTALL
 rm webmin_*  #remove any webmin .deb files
 sudo apt install -y $DEPENDENCIES_LIST
+echo 'change to /tmp directory...'
+cd /tmp
 wget https://sourceforge.net/projects/webadmin/files/webmin/${VERSION}/webmin_${VERSION}_all.deb
 sudo dpkg -i webmin_${VERSION}_all.deb 
+echo 'clear installation files...'
+rm webmin_${VERSION}_all.deb
+echo '\nDONE!\n\n'
